389829c52b53b128f5ac581b27c32b197fff538f
kaziabulhasib <abulhasibkazi@gmail.com>
2024-02-18 14:52:23 +0530

    ticket number and price display

--- Script/script.js
+++ Script/script.js
@@ -2,11 +2,12 @@ function goToSection() {
   window.location.href = "#ph-paribahan";
 }
 
-// but click event
+// button click event
 
 const bookButtons = document.getElementsByClassName("seat-button");
 let setBookCount = 0;
 let seatLeft = 40;
+let bookDetailsContainer = document.getElementById("book-details-container");
 
 for (const bookButton of bookButtons) {
   bookButton.addEventListener("click", function () {
@@ -14,11 +15,26 @@ for (const bookButton of bookButtons) {
     bookButton.style.color = "white";
     bookButton.disabled = true;
 
-    // seting the value of seat book count
+    // seting the value of seat book count & seat left
     setBookCount++;
-    console.log(setBookCount);
+    // console.log(setBookCount);
     document.getElementById("seat-book-count").innerText = setBookCount;
     seatLeft--;
     document.getElementById("seat-left").innerText = seatLeft;
+
+    // adding book details in the container
+    const div = document.createElement("div");
+    const p1 = document.createElement("p");
+    const p2 = document.createElement("p");
+    const p3 = document.createElement("p");
+    p1.innerText = bookButton.innerText;
+    p2.innerText = "Economoy";
+    p3.innerText = "550";
+    div.appendChild(p1);
+    div.appendChild(p2);
+    div.appendChild(p3);
+    div.setAttribute("class", "seat-class-price");
+
+    bookDetailsContainer.appendChild(div);
   });
 }
